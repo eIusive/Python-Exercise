fd070ce9c5c893605431256936a44566cb39dc6d
phatmaggie <erikedwards37@gmail.com>
2021-04-12 22:55:24 -0400

    Add files via upload

---
+++ lab11.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# Erik Edwards Lab 11
+
+def words():
+
+	word1 = "Stapler before dawn"
+	word2 = "Waiting in perfect silence"
+	word3 = "Dreams of loose paper"
+
+	poem = word1 + "\n" + word2 + "\n" + word3
+
+	print poem
+
+if __name__ == '__main__':
+	words()
